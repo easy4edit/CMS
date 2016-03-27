d5a7122d03bf742719c3b139e0e79d1e3b969cdf
Jan Nový <honza.novy@gmail.com>
2016-03-27 18:00:57 +0200

    Router pro adminModule

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -6,18 +6,23 @@ use Nette;
 use Nette\Application\Routers\RouteList;
 use Nette\Application\Routers\Route;
 
+class RouterFactory {
 
-class RouterFactory
-{
+    /**
+     * @return Nette\Application\IRouter
+     */
+    public static function createRouter() {
+        $router = new RouteList;
+        $router[] = new Route('admin/<presenter>/<action>/<id>', array(
+            'module' => 'Admin',
+            'presenter' => 'Default',
+            'action' => 'default',
+            'id' => NULL,
+        ));
 
-	/**
-	 * @return Nette\Application\IRouter
-	 */
-	public static function createRouter()
-	{
-		$router = new RouteList;
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
-		return $router;
-	}
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+
+        return $router;
+    }
 
 }
